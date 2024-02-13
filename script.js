034b08e86c86958c6f7f0419959226313d9db644
gilberto barbato <gibarbato@gmail.com>
2024-02-12 21:38:13 -0300

    inserindo novo index e estilos

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 var tarefas = [
-    { nome: "aula Fullstack",
-      tipo: "pessoal"  
-    },
-    { nome: "planejamento",
-      tipo: "trabalho"  
-    },
-    { nome: "reunião",
-      tipo: "trabalho"  
-    },
-    { nome: "supermercado",
-      tipo: "pessoal"  
-    },
+  {
+    nome: "aula Fullstack",
+    tipo: "pessoal"
+  },
+  {
+    nome: "planejamento",
+    tipo: "trabalho"
+  },
+  {
+    nome: "reunião",
+    tipo: "trabalho"
+  },
+  {
+    nome: "supermercado",
+    tipo: "pessoal"
+  },
 ];
 
 console.log(tarefas);
 
 function exibirTarefas() {
-    var listaInicial = document.getElementById("lista-tarefas");
+  var listaInicial = document.getElementById("lista-tarefas");
 
-    // Adiciona cada tarefa a listaInicial
-    tarefas.forEach(function (tarefa, index) {
-        setTimeout(function () {
-            var tarefaDiv = document.createElement("li");
-            tarefaDiv.innerHTML = `
-               <input id="item-${index}" type="checkbox" class="form-check-input check-tarefa"/> 
-                <label for="item-${index} class="checkbox-wrapper">${tarefa.nome} - ${tarefa.tipo}</label>
-               <button class="btn btn-outline-danger">x</button>
+  // Adiciona cada tarefa a listaInicial
+  tarefas.forEach(function (tarefa, index) {
+    setTimeout(function () {
+      var tarefaDiv = document.createElement("ul");
+      tarefaDiv.innerHTML = `
+        <li>
+        <input type="checkbox" id="item-${index}"> <label for="item-${index}" class="checkbox-wrapper">${tarefa.nome} - ${tarefa.tipo}</label> <button class="btn-cancel">x</button>
+        </li>
     `;
-            listaInicial.appendChild(tarefaDiv);
-        }, 500 * index)
-    });
+      listaInicial.appendChild(tarefaDiv);
+    }, 500 * index)
+  });
 }
 
 exibirTarefas();
